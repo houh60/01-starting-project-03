a5e55af6f72e15b19bf0af05e3da98b12aac8326
William Hou <williamhou38@yahoo.com>
2026-06-29 02:08:38 -0500

    new changes

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,12 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  inject,
+  input,
+  OnInit,
+  ViewEncapsulation,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,7 +15,16 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
+  host: { class: 'control', '(click)': 'onClick()' },
 })
-export class ControlComponent {
+export class ControlComponent implements OnInit {
   label = input.required<string>();
+  labelFor = input.required<string>();
+  private el = inject(ElementRef);
+
+  ngOnInit(): void {}
+
+  onClick() {
+    console.log('clicked!');
+  }
 }
